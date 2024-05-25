76b171455a79d4a9e4b43a62064910d8b48c3ade
rediat <rediat_ta@ch.iitr.ac.in>
2024-05-25 22:03:14 +0530

    minor adjustments

--- server/model/table.js
+++ server/model/table.js
@@ -1,1 +1,10 @@
-export let studentSubmissionForm = ``
+// export let studentSubmissionForm = ``
+
+
+//  table and there information 
+
+    // 1.instructor name table
+    // 2.batch table
+    // 3.project name table
+    // 4.project link table
+    // 4.group table ( generally group will be maximum 4 excluding self study group) 
